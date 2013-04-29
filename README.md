0f2b023f8ca4feb4dab905767ada4d1086e06344
Jerad Bitner <sirkitree@gmail.com>
2013-04-28 20:56:41 -0600

    new name for directive

---
+++ README.md
@@ -0,0 +1,36 @@
+# Angular Google Plus Sign-in Button Directive
+
+
+[Homepage](http://sirkitree.github.com/angular-directive.g+signin/)
+
+
+A good starting place for a Google Plus sign-in button. Specify your client id and you're done. There's also support for overriding the data-* options found here: [ ]
+
+## Usage
+1. Include `google-plus-signin.js`.
+2. Add `directive.g+signin` as a dependency to your app.
+3. Add `<g+signin clientid="your-client-id">` to your app.
+
+<!-- uncomment once available
+## Bower
+Installable via `bower`:
+
+```bash
+bower install angular-directive.g+signin
+```
+-->
+
+## Example
+ 
+See the [homepage](http://sirkitree.github.com/angular-directive.g+signin/) for an example. 
+
+
+```html
+<div ng-app="directive.g+signin">
+  <g+signin clientid="620125449078"></g+signin>
+  <p>^ This is a Google Plus sign-in button</p>
+</div>
+```
+
+## License
+♡ CopyHeart 2013 by [Jerad Bitner](http://jeradbitner.com) | Copying is an act of love. Please copy.
---
+++ google-plus-signin.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/*
+ * angular-google-plus-directive v0.0.1
+ * ♡ CopyHeart 2013 by Jerad Bitner http://jeradbitner.com
+ * Copying is an act of love. Please copy.
+ */
+
+angular.module('directive.g+signin', []).
+  directive('g+signin', function () {
+    return {
+      restrict: 'E',
+      template: '<span></span>',
+      replace: true,
+      link: function (scope, element, attrs) {
+        
+        // @todo: write about the signinCallback
+        attrs.$set('data-callback', 'signinCallback');
+        attrs.$set('data-clientid', attrs.clientid + '.apps.googleusercontent.com');
+        attrs.$set('data-cookiepolicy', 'single_host_origin');
+        attrs.$set('data-requestvisibleactions', 'http://schemas.google.com/AddActivity');
+        attrs.$set('data-scope', 'https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email');
+        attrs.$set('data-width', 'wide');
+
+        (function() {
+          var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+          po.src = 'https://apis.google.com/js/client:plusone.js';
+          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+        })();
+      }
+    };
+  });
