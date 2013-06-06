e7a121f0d99c0f86a2774ce7bd502983a28f4897
Doug Bitting <doug@bittingclan.com>
2013-06-06 07:44:07 -0700

    Actually, now that I look at it more closely, attributes on the declaration are carried over onto the directive's template.  That simplifies some things.  Now, explicitly set class and data-clientid and simply default the other attributes that existed in previous versions.  This should allow for explicit setting of supported attributes without requiring the directive to get involved.

--- google-plus-signin.js
+++ google-plus-signin.js
@@ -14,40 +14,26 @@ angular.module('directive.g+signin', []).
       replace: true,
       link: function (scope, element, attrs) {
         
+        // Set the class on the element.  This has to be special cased, since it's a real HTML attribute.
+        attrs.$set('class', 'g-signin');
+
+        attrs.$set('data-clientid', attrs.clientid + '.apps.googleusercontent.com');
+        delete attrs.clientid; // Avoid duplication and potential confusion
+
         // Some default values, based on prior versions of this directive
         var options = {
           callback: 'signinCallback',
-          class: 'g-signin',
-          clientid: attrs.clientid + '.apps.googleusercontent.com',
           cookiepolicy: 'single_host_origin',
           requestvisibleactions: 'http://schemas.google.com/AddActivity',
           scope: 'https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email',
           width: 'wide'
         };
 
-        // These are the attribute names supported by the Google+ Login API
-        var attrNames = ['class', 'cookiepolicy', 'accesstype', 'apppackagename',
-            'approvalprompt', 'callback', 'height', 'redirecturi', 'requestvisibleactions', 'scope',
-            'theme', 'width'
-        ];
-
-        // Override defaults with values provided via attribute declarations
-        angular.forEach(attrNames, function(elt) {
-          if (attrs.hasOwnProperty(elt)) {
-            options[elt] = attrs[elt];
-          } else if (attrs.hasOwnProperty('data-' + elt)) {
-            // If the attrs already has the data-* property, we don't need anything
-            delete options[elt];
-          }
-        });
-
-        // Set the class on the element.  This has to be special cased, since it's a real HTML attribute.
-        attrs.$set('class', options.class);
-        delete options.class;
-
         // Set the data-* attributes on the element, based on the defaults and information provided by
         angular.forEach(Object.getOwnPropertyNames(options), function(elt) {
-          attrs.$set('data-' + elt, options[elt]);
+          if (!(attrs.hasOwnProperty(elt) || attrs.hasOwnProperty('data-' + elt))) {
+            attrs.$set('data-' + elt, options[elt]);
+          }
         });
 
         // Asynchronously load the G+ SDK.
